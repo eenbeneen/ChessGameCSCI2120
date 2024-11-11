0dc22fbcf86b5a8f5289352e690c6eb2f6e9869e
eenbeneen <gebremichaeleben@gmail.com>
2024-11-11 12:56:42 -0500

    Added classes for pieces, wrote function for rooks
    There is an error where every possible coordinate is added twice

--- main.cpp
+++ main.cpp
@@ -1,5 +1,113 @@
 #include <iostream>
+#include <vector>
 
+//Function to easily convert 2 coordinates to a vector
+std::vector<int> convertToVector(int x, int y) {
+	std::vector<int> vec = { x, y };
+	return vec;
+}
+
+class Piece {
+public:
+	//Position variables that can be 0-7
+	int xPos = 0;
+	int yPos = 0;
+	//Boolean thats true if the piece is white, false if black
+	bool isWhite;
+	//Get moves function that currently returns an empty vector of int vectors
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+		std::vector<std::vector<int>> possibleMoves;
+		return possibleMoves;
+	}
+
+};
+
+class Pawn: public Piece {
+public:
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+		std::vector<std::vector<int>> possibleMoves;
+
+	}
+};
+
+class Rook : public Piece {
+public:
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+
+		
+		std::vector<std::vector<int>> possibleMoves;
+		
+		//This code checks the rook's y (vertical) path
+		//The for loop runs twice, once with dir = 1 and once with dir = -1
+		for (int dir = 1; dir >= -1; dir -= 2) {
+			//This for loop checks every space in front of the rook when dir = 1
+			//and every space behind the rook when dir = -1
+			for (int i = xPos + dir; i < 8 && i >= 0; i+=dir) {
+				
+				if (board[i][yPos] == 0) {
+					//If the space is 0, then the rook can move there, add it to the pos. moves
+					possibleMoves.push_back(convertToVector(i, yPos));
+				}
+				else {
+					//If not, break the loop
+					//Add checking for captures later
+					break;
+				}
+
+			}
+		}
+
+		//Same here, but the for loop checks the rooks path along x
+		for (int dir = 1; dir >= -1; dir -= 2) {
+			for (int i = yPos + dir; i < 8 && i >= 0; i+=dir) {
+				if (board[xPos][i] == 0) {
+					possibleMoves.push_back(convertToVector(xPos, i));
+				}
+				else {
+					break;
+				}
+
+			}
+		}
+
+		return possibleMoves;
+		
+	}
+};
+
+class Knight : public Piece {
+public:
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+		std::vector<std::vector<int>> possibleMoves;
+
+	}
+};
+
+class Bishop : public Piece {
+public:
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+		std::vector<std::vector<int>> possibleMoves;
+
+	}
+};
+
+class Queen : public Piece {
+public:
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+		std::vector<std::vector<int>> possibleMoves;
+
+	}
+};
+
+class King : public Piece {
+public:
+	std::vector<std::vector<int>> getMoves(char board[][8]) {
+		std::vector<std::vector<int>> possibleMoves;
+
+	}
+};
+
+	
 //This function prints the board to the output window in proper format
 void showBoard(char board[][8]) {
 	
@@ -26,11 +134,11 @@ int main() {
 
 	//The starting chess board
 	/*
-		Uppercase: white
-		Lowercase: black
+		UPPERCASE: WHITE
+		lowercase: black
 		P = pawn
 		R = rook
-		N = knight
+		N = knight	
 		B = bishop
 		Q = queen
 		K = king
@@ -38,7 +146,7 @@ int main() {
 	char board[8][8] = {
 
 		{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{0, 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
 		{0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0},
@@ -50,6 +158,21 @@ int main() {
 
 
 	showBoard(board);
+
+	Rook rook;
+
+	std::vector<std::vector<int>> rookMoves = rook.getMoves(board);
+	std::cout << "\n ROOKMOVES SIZE: " << rookMoves.size() << " \n";
+	for (int i = 0; i < rookMoves.size(); i++) {
+		
+		for (int j = 0; j < rookMoves.at(i).size(); j++) {
+			std::cout << "(";
+			std::cout << rookMoves.at(i).at(0) << ", " << rookMoves.at(i).at(1);
+			std::cout << ")\n";
+		}
+		
+	}
+
 
 
 
